7e3c0e203568b192cae4d0475891eb7216f53fa1
abdelmoaize <abdelmouaize@gmail.com>
2022-03-25 23:38:23 +0100

    Done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+let gameover = document.querySelector("#gameover")
 let ball = document.querySelector("#ball");
-let bx = Math.floor(Math.random()*100);
+let bx = Math.floor(Math.random()*90 + 5);
 let by = 0;
 ball.style.left = bx + "%";
 let racket = document.querySelector("#racket");
@@ -8,36 +9,46 @@ let scoreBox = document.querySelector("#score")
 let score = 0;
 let speedInput = document.querySelector("#speed")
 let speed = speedInput.value
-console.log(speed)
 
-function randby(){
-    bx = Math.floor(Math.random()*90 + 5)
-}
-
-setInterval(()=>{
+function gameplay() {
+    speed = speedInput.value
     if (by>=90 && bx>rx-5 && bx<rx+20){
         hit()
     }
-    update()
-},10)
-
-function update() {
-    speed = speedInput.value
-    if (by==0){
-        randby()
+    else if(by >= 93){
+        console.log(by)
+        lost()
     }
-    by = by+speed*0.1
-    by=by%95
+    by = by+speed*0.1;
     ball.style.left = bx + "%"
     ball.style.top = by + "%"
     racket.style.left= rx + "%"
     scoreBox.innerText = score;
 }
+let Play = setInterval(gameplay,10)
 
+function randby(){
+    bx = Math.floor(Math.random()*90 + 5)
+}
 function hit() {
+    randby()
     by=0
     score += 1
     console.log(score)
+}
+function lost() {
+    clearInterval(Play)
+    gameover.innerHTML=`<button id="replay" onclick="replay()">Play Again !</button>`
+    gameover.classList.add("gameover")
+}
+function replay(){
+    gameover.classList.remove("gameover")
+    gameover.innerHTML=""
+    score=0;
+    by=0;
+    randby()
+    rx=40
+    Play = setInterval(gameplay,10)
 }
 
 
